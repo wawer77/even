7cad8d8884a7b2578b74e0bfad4664dd4c3422e3
wawer77 <tomek.06@wp.pl>
2019-07-31 00:46:38 +0200

    Work in progress on Balance: create meth.

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -1,5 +1,5 @@
 class BalancesController < ApplicationController
-  
+
   def index
   end
 
@@ -9,6 +9,9 @@ class BalancesController < ApplicationController
 
   def create
     @balance = Balance.new(balance_params)
+    #balancer_1_id = current_user
+    @balancer_1_id = 1
+    @balancer_2_id = 2
     if @balance.save
       redirect_to @balance, notice: "Your balance was created successfully"
     else
@@ -32,7 +35,7 @@ class BalancesController < ApplicationController
   private
 
   def balance_params
-    params.require(:balance).permit(:balancer_1, :balancer_2, :balancer_1_debt, :balancer_2_debt)
+    params.require(:balance).permit(:balancer_1_id, :balancer_2_id, :balancer_1_debt, :balancer_2_debt)
   end
 
 end
