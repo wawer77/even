c708b143821acebc4ad43f3a3687acf47d73c72c
wawer77 <tomek.06@wp.pl>
2019-12-08 01:52:49 +0100

    Clean up model files

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -1,6 +1,4 @@
 class Balance < ApplicationRecord
-  #should not be optional!
   belongs_to :owner, class_name: 'User'
   belongs_to :added_user, class_name: 'User'
-#  belongs_to :users, class_name: 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates_presence_of :first_name, :last_name, :username
 
-  #has_many  :started_balances, :class_name => 'Balance', :foreign_key => 'balancer_1_id'
-  #has_many  :added_to_balances, :class_name => 'Balance', :foreign_key => 'balancer_2_id'
   has_many :own_balances, as: :owner, class_name: 'Balance'
   has_many :added_to_balances, as: :added_user, class_name: 'Balance'
 end
