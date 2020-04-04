400b38a00de91822489de5571adcf8a20466c83a
wawer77 <tomek.06@wp.pl>
2020-04-04 19:02:12 +0200

    Create situation and otheruser Balance helper methods

--- app/helpers/balances_helper.rb
+++ app/helpers/balances_helper.rb
@@ -1,2 +1,32 @@
 module BalancesHelper
+
+  def otheruser(balance)
+      if balance.owner_id == current_user.id
+        User.find(balance.added_user_id).username
+      else
+        User.find(balance.owner_id).username
+      end
+    end
+
+  def situation(balance)
+   if balance.owner_id == current_user.id
+      if balance.value > 0
+       statement="You lended"
+      elsif balance.value < 0
+        statement="You owe"
+      else
+        statement="You are even!"
+      end
+    else
+      if balance.value > 0
+        statement="You owe"
+      elsif balance.value < 0
+        statement="You lended"
+      else
+        statement="You are even!"
+      end
+    end
+    statement + " " + (balance.value unless balance.value == 0).to_s
+  end
+
 end
