f00b59373e41bed2953e630032a7f94f049d7159
wawer77 <tomek.06@wp.pl>
2020-04-12 01:16:00 +0200

    Modify routes; add Transactions controller methods.

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,41 @@
 class TransactionsController < ApplicationController
+    def index
+        #@own_transactions = transaction.where(owner_id: current_user.id)
+        #@added_to_transactions = transaction.where(added_user_id: current_user.id)
+        #@transactions = @own_transactions + @added_to_transactions
+    end
+  
+    def new
+      @transaction = Transaction.new
+    end
+  
+    def create
+      @transaction = transaction.new(transaction_params)
+      @transaction.issuer_id = current_user.id
+      if @transaction.save
+        redirect_to @transaction, notice: "Your transaction was created successfully!"
+      else
+        render :new
+      end
+    end
+  
+    def show
+      @transaction = transaction.find(params[:id])
+    end
+  
+    def edit
+    end
+  
+    def update
+    end
+  
+    def destroy
+    end
+  
+    private
+  
+    def transaction_params
+      params.require(:transaction).permit(:description, :value , :issuer_id, :receiver_id, :send_money, :balance_id)
+    end
+  
  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :balances
+  resources :transactions
   root 'static#landing'
 end
