7cfb1b70b7882128233342c9bad1db6b1b5f4a93
wawer77 <tomek.06@wp.pl>
2020-04-27 22:50:16 +0200

    Rearrange Balances controller

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -21,7 +21,6 @@ class BalancesController < ApplicationController
     #Shouldn't the line above be in the block below?
     if @balance.save
       @balance.users << [@creator, @partner]
-      pry
       redirect_to @balance, notice: "Your balance was created successfully!"
     else
       render :new
@@ -34,7 +33,6 @@ class BalancesController < ApplicationController
     @balance_transactions = @balance.transactions
 
     #####lending transactions:
-    @current_user_lending_transactions = Array.new
     @current_user_lending_transactions = @balance_transactions.where(["issuer_id = ? and send_money = ?", current_user.id, 'true']) + @balance_transactions.where(["issuer_id = ? and send_money = ?", @balance_partner.id, 'false'])
 
     @current_user_lended_value = 0
@@ -43,7 +41,6 @@ class BalancesController < ApplicationController
     end
     
     #####borrowing transactions:
-    @current_user_borrowing_transactions = Array.new
     @current_user_borrowing_transactions = @balance_transactions.where(["issuer_id = ? and send_money = ?", current_user.id, 'false']) + @balance_transactions.where(["issuer_id = ? and send_money = ?", @balance_partner.id, 'true'])
 
     @current_user_borrowed_value = 0
@@ -53,15 +50,24 @@ class BalancesController < ApplicationController
 
     #####balance satus:
     if @current_user_lended_value > @current_user_borrowed_value
-      @balance_status = { status: "lending", value: @current_user_lended_value - @current_user_borrowed_value, message: "You lended: " }
+      @balance_status = { 
+        status: "lending",
+        value: @current_user_lended_value - @current_user_borrowed_value, 
+        message: "You lended: " 
+      }
     elsif @current_user_lended_value < @current_user_borrowed_value
-      @balance_status = { status: "owing", value: @current_user_borrowed_value - @current_user_lended_value, message: "You owe: " }
+      @balance_status = { 
+        status: "owing", 
+        value: @current_user_borrowed_value - @current_user_lended_value,
+        message: "You owe: " 
+      }
     else
-      @balance_status = { status: "even", value: nil, message: "You are even!" }
+      @balance_status = { 
+        status: "even", 
+        value: nil, 
+        message: "You are even!" 
+    }
     end
-    
-    #pry
-    
   end
 
   def edit
