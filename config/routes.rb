5cfe452fe383a9b393321baa74b65a069936a560
wawer77 <tomek.06@wp.pl>
2020-05-04 01:18:05 +0200

    Create Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :users, only: [:show]
   resources :transactions, :balances
   root 'static#landing'
 end
