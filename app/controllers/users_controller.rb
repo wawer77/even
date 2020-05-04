593495f5a6be4156d8ffed5f0044d5fde25c580e
wawer77 <tomek.06@wp.pl>
2020-05-04 23:57:18 +0200

    Add Users show method and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+    def show
+        @user = User.find(params[:id])
+        @transactions = transactions_with(@user, current_user)
+        @transactions_output = transactions_output(@transactions, current_user)
+        @balances = @transactions.collect(&:balance).uniq.sort_by{|t| t[:created_at]}.reverse
+        @balances_output = balances_output(@balances, current_user)
+    end
+
+    private  
+    def transactions_with(user, partner)
+        user.transactions.where(issuer_id: partner.id) + user.transactions.where(receiver_id: partner.id)
+    end
 end
