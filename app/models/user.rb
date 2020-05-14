1c0d4ff08e384ca9895bb1cdd62b4b802cbe124f
wawer77 <tomek.06@wp.pl>
2020-05-14 18:03:40 +0200

    Fruitless messing with Users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,12 @@ class User < ApplicationRecord
   has_many :issued_transactions, foreign_key: :issuer, class_name: 'Transaction'
   has_many :received_transactions, foreign_key: :receiver, class_name: 'Transaction' 
   has_and_belongs_to_many :balances
+  #has_many :transactions, ->(user) { where("issuer_id = ? OR receiver_id = ?", user.id, user.id) }
+  #not working as it cheks for transactions WHERE user_ud = user.id AND the conditions above
+  #https://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html#module-ActiveRecord%3a%3aAssociations%3a%3aClassMethods-label-Customizing+the+query
 
-  ####### This method is defined as couldn't extend the relation to get transactions including both types; This reutrn an array instead of Relation anyway (so cannot .where it)
+  ####### This method is defined as couldn't extend the relation to get transactions including both types; 
+  #This reutrn an array instead of Relation anyway (so cannot .where it) - don't remember what I meant by this tho
   def transactions
     Transaction.where( 'issuer_id = :id OR receiver_id = :id', id: "#{ self.id }" )
   end 
