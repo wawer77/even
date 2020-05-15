683ed3980b2766ee2461e36186fe89512597667c
wawer77 <tomek.06@wp.pl>
2020-05-15 22:05:53 +0200

    Add sent/received invitation relation to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,14 @@ class User < ApplicationRecord
   has_many :received_transactions, foreign_key: :receiver, class_name: 'Transaction' 
   has_and_belongs_to_many :balances
 
-  has_many :friendships, dependent: :destroy
+  has_many :friendships, -> { where(status: "confirmed") }, dependent: :destroy
   has_many :friends, through: :friendships
+  
+  has_many :sent_invitations, ->(user) { where(status: "pending", invitor_id: user.id) }, foreign_key: :user, class_name: 'Friendship'
+  has_many :received_invitations, ->(user) { where(status: "pending").where.not(invitor_id: user.id) }, foreign_key: :user, class_name: 'Friendship'
 
+
+  #! TRY JOIN?
   #has_many :transactions, ->(user) { where("issuer_id = ? OR receiver_id = ?", user.id, user.id) }
   #not working as it cheks for transactions WHERE user_ud = user.id AND the conditions above
   #https://api.rubyonrails.org/classes/ActiveRecord/Associations/ClassMethods.html#module-ActiveRecord%3a%3aAssociations%3a%3aClassMethods-label-Customizing+the+query
