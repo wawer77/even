6760033cba753cc8e580208868aeff7ded24e0dc
wawer77 <tomek.06@wp.pl>
2020-05-17 22:17:31 +0200

    Add overall status to user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
         @transactions_output = transactions_output(@transactions, current_user)
         @balances = @transactions.collect(&:balance).uniq.sort_by{|t| t[:created_at]}.reverse
         @balances_output = balances_output(@balances, current_user)
+        @overall_status = balance_status(@balances, current_user)   
     end
 
     private  
