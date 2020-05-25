cc13698eff747aa6c69b3853da33ba24ec5218c1
wawer77 <tomek.06@wp.pl>
2020-05-26 00:39:13 +0200

    Add creator_id column to Transactions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_25_203342) do
+ActiveRecord::Schema.define(version: 2020_05_25_215131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_05_25_203342) do
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
     t.integer "updated_by_id"
+    t.integer "creator_id"
   end
 
   create_table "users", force: :cascade do |t|
