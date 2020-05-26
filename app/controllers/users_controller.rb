ac3044494abd5bd1dbc3b4c5aac3536f7e39be71
wawer77 <tomek.06@wp.pl>
2020-05-26 23:58:41 +0200

    Apply Transaction sorting to User show and refactor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
-        @transactions = current_user.transactions_with(@user)
-        @transactions_output = transactions_output(@transactions, current_user)
-        @balances = current_user.balances_with(@user).sort_by{|t| t[:created_at]}.reverse
-        @balances_output = balances_output(@balances, current_user)
+        @sorted_transactions = transactions_sorted(current_user.transactions_with(@user))
+        @transactions_output = transactions_output(@sorted_transactions, current_user).reverse.to_enum.with_index(1)
+        @balances_output = balances_output(current_user.balances_with(@user), current_user)
         @overall_status = overall_status_with(@user)   
     end
-
-    private  
 end
