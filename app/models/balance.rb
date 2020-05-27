fd637856b2ad2ff14b4fbe68b92328dbe177afb3
wawer77 <tomek.06@wp.pl>
2020-05-28 00:55:19 +0200

    Refactor Balace-Transaction association

--- app/models/balance.rb
+++ app/models/balance.rb
@@ -3,18 +3,14 @@ class Balance < ApplicationRecord
     validates_presence_of :name
     #valdation turned off for update, as must provide partner_id(which exists already) when changing updated_by_id whenever transaction is created->
     validates_presence_of :partner_id, on: :create
-    #has_many :transactions - try to make it work?
     
+    has_many :transactions, class_name: 'Transaction', foreign_key: 'balance_id', dependent: :delete_all
     has_and_belongs_to_many :users
     belongs_to :creator, class_name: 'User'
     belongs_to :editor, class_name: 'User'
 
     def partner_for(user)
         self.users.where.not(id: user.id).first
-    end
-
-    def transactions
-        Transaction.where(balance_id: self.id)
     end
 
     def creator
