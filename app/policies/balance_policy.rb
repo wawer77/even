832b1e33783e368eed67af889e1810591eed788b
wawer77 <tomek.06@wp.pl>
2020-05-28 16:57:42 +0200

    Add Balance authorization

--- app/policies/balance_policy.rb
+++ app/policies/balance_policy.rb
@@ -1,5 +1,20 @@
 class BalancePolicy < ApplicationPolicy
+  def show?
+    record_includes_user?
+  end
+  def update?
+    record_includes_user?
+  end
+
+  def edit?
+    update?
+  end
+
   def destroy?
+    record_includes_user?
+  end
+
+  def record_includes_user?
     record.users.include?(user)
   end
 end
