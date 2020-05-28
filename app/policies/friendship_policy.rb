2d2d1360651d7eaa6cdf891c261e6f96c052c1be
wawer77 <tomek.06@wp.pl>
2020-05-28 16:58:03 +0200

    Add Friendship authorization

--- app/policies/friendship_policy.rb
+++ app/policies/friendship_policy.rb
@@ -2,4 +2,8 @@ class FriendshipPolicy < ApplicationPolicy
   def confirm?
     record.user_id == user.id && record.user_id != record.invitor_id
   end
+
+  def destroy?
+    record.user_id == user.id
+  end
 end
