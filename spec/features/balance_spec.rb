3e3794d0dfcf66935941c36da4c788c7336abd8a
wawer77 <tomek.06@wp.pl>
2022-01-11 20:41:03 +0100

    Add Balance index list tests

--- spec/features/balance_spec.rb
+++ spec/features/balance_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe 'navigate' do
   before do
-    user = FactoryBot.create(:user)
-    login_as user
+    @user = FactoryBot.create(:user)
+    login_as @user
   end
   
   describe 'index' do
@@ -11,6 +11,20 @@ describe 'navigate' do
       visit balances_path
       expect(page.status_code).to eq(200) #with no sign in, this is redirected to login and code is 200 nevertheless
       expect(page).to have_content(/Balances/)
+    end
+
+    it 'lists balances created by the user' do
+      balance = FactoryBot.create(:balance, creator_id: @user.id)
+      balance.users << [User.find(balance.creator_id),User.find(balance.partner_id)] #not sure it should be added here, but it works
+      visit balances_path
+      expect(page).to have_link(:href => balance_path(balance))
+    end
+    
+    it 'lists balances the user was added to' do
+      balance = FactoryBot.create(:balance, partner_id: @user.id)
+      balance.users << [User.find(balance.creator_id),User.find(balance.partner_id)] #not sure it should be added here, but it works
+      visit balances_path
+      expect(page).to have_link(:href => balance_path(balance))
     end
   end
 
@@ -20,5 +34,7 @@ describe 'navigate' do
       expect(page.status_code).to eq(200)
       expect(page).to have_content(/New balance/)
     end
+
+    # Add test for filling the form and creating the balance
   end
 end
