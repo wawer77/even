6c5a46a5d937bb0a2f9e9bad52e162beea4a759c
wawer77 <tomek.06@wp.pl>
2022-01-14 04:20:06 +0100

    Add tests to Balance features

--- spec/features/balance_spec.rb
+++ spec/features/balance_spec.rb
@@ -7,6 +7,10 @@ describe 'navigate' do
   end
   
   describe 'index' do
+    before do
+      @balance = FactoryBot.create(:balance, creator_id: @user.id)
+      @balance.users << [User.find(@balance.creator_id),User.find(@balance.partner_id)] #not sure it should be added here, but it works
+    end
     it 'can be reached successfully' do
       visit balances_path
       expect(page.status_code).to eq(200) #with no sign in, this is redirected to login and code is 200 nevertheless
@@ -14,10 +18,14 @@ describe 'navigate' do
     end
 
     it 'lists balances created by the user' do
-      balance = FactoryBot.create(:balance, creator_id: @user.id)
-      balance.users << [User.find(balance.creator_id),User.find(balance.partner_id)] #not sure it should be added here, but it works
       visit balances_path
-      expect(page).to have_link(:href => balance_path(balance))
+      expect(page).to have_link(:href => balance_path(@balance))
+    end
+
+    it 'balance link works' do
+      visit balances_path
+      click_link('show-link')
+      expect(page).to have_current_path(balance_path(@balance))
     end
     
     it 'lists balances the user was added to' do
@@ -25,6 +33,32 @@ describe 'navigate' do
       balance.users << [User.find(balance.creator_id),User.find(balance.partner_id)] #not sure it should be added here, but it works
       visit balances_path
       expect(page).to have_link(:href => balance_path(balance))
+    end
+
+    describe 'Edit Balance button' do
+      it 'exists' do
+        visit balances_path
+        expect(page).to have_link(:href => edit_balance_path(@balance))
+      end
+
+      it 'works' do
+        visit balances_path
+        click_link("Edit balance")
+        expect(page).to have_current_path(edit_balance_path(@balance))
+      end
+    end
+      
+    describe 'Add Transaction button' do
+      it 'exists' do 
+        visit balances_path
+        expect(page).to have_link(:href => new_transaction_path)
+      end
+
+      it 'works' do
+        visit balances_path
+        click_link("Add transaction")
+        expect(page).to have_current_path("/transactions/new?balance_id=#{@balance.id}")
+      end
     end
   end
 
